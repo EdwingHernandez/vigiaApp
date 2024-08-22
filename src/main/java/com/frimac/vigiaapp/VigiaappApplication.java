388c67ccb0c825700044843dd364317fbac2bd4c
EdwingDuvanHernandezHerrera <124322869+EdwingDuvanHernandezHerrera@users.noreply.github.com>
2024-08-22 15:33:18 -0500

    feat :tada: Iniicio del proyecto

---
+++ src/main/java/com/frimac/vigiaapp/VigiaappApplication.java
@@ -0,0 +1,13 @@
+package com.frimac.vigiaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigiaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigiaappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frimac/vigiaapp/VigiaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frimac.vigiaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigiaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
